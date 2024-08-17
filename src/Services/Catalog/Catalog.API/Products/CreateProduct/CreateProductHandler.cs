9904482af6097482d3e3367fd49070d00574a56e
Linda Mazibuko <linda.mazibuko@aat.co.za>
2024-08-17 02:01:05 +0200

    Abstract Mediatr for CQRS, Implement handler and endpoint classes

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,8 +1,28 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
 {
-    public record CreateProductCommand(string Name,List<string> Category, string Description, string ImageFile, decimal Price);
+    public record CreateProductCommand(string Name,List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    public class CreateProductHandler
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
+        public async Task<CreateProductResult> Handle(CreateProductCommand productCommand, CancellationToken cancellationToken)
+        {
+            //create product entity from command object
+            var product = new Product
+            {
+                Name = productCommand.Name,
+                Category = productCommand.Category,
+                Description = productCommand.Description,
+                ImageFile = productCommand.ImageFile,
+                Price = productCommand.Price,
+            };
+            //save to database
+
+            //return CreateProductResult result
+            return new CreateProductResult(Guid.NewGuid());
+        }
     }
 }
